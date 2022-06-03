01c2bae02c1190d0fae105016b8b6b479d6d4ccb
ShirinBazis <bazis.shirini@gmail.com>
2022-06-04 00:53:41 +0300

    login and register screens

---
+++ app/src/main/java/com/example/ex3/form/Login.java
@@ -0,0 +1,34 @@
+package com.example.ex3.form;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ex3.R;
+
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    boolean isAllFieldsChecked = false;
+    EditText etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // the button that will transfer to the register page
+        Button btnGoToRegister = findViewById(R.id.btnGoToRegister);
+        btnGoToRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Register.class);
+            startActivity(intent);
+        });
+        // the button that will check whether the details of the user are correct
+
+    }
+
+    private boolean CheckAllFields() {
+        // need to check if this user really exists
+        // need to check if this is the right password of the user
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/ex3/form/Register.java
@@ -0,0 +1,72 @@
+package com.example.ex3.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.ex3.R;
+
+public class Register extends AppCompatActivity {
+
+    boolean isAllFieldsChecked = false;
+    EditText etUsername, etNickname, etPassword, etConfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        etUsername = findViewById(R.id.editTextUsername);
+        etNickname = findViewById(R.id.editTextNickname);
+        etPassword = findViewById(R.id.editTextPassword);
+        etConfirm = findViewById(R.id.editTextConfirmPassword);
+        // the button that will transfer to the login page
+        Button btnGoToLogin = findViewById(R.id.btnGoToLogin);
+        btnGoToLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(this, com.example.androidapp.form.Login.class);
+            startActivity(intent);
+        });
+        btnRegister.setOnClickListener(v -> {
+            // if all the necessary details correctly inserted, register the user
+            isAllFieldsChecked = CheckAllFields();
+            if (isAllFieldsChecked) {
+                Intent intent = new Intent(this, com.example.androidapp.form.Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean CheckAllFields() {
+        int flag = 0;
+        if (etUsername.length() == 0) {
+            etUsername.setError("This field is required");
+            flag = 1;
+        }
+        if (etNickname.length() == 0) {
+            etNickname.setError("This field is required");
+            flag = 1;
+        }
+        if (etPassword.length() == 0) {
+            etPassword.setError("Password is required");
+            flag = 1;
+        }
+        if (!etPassword.getText().toString().matches("^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$")) {
+            etPassword.setError("Password must contain letters and numbers");
+            flag = 1;
+        }
+        if (etPassword.length() < 8) {
+            etPassword.setError("Password must be minimum 8 characters");
+            flag = 1;
+        }
+        if (etConfirm.length() == 0) {
+            etConfirm.setError("You must confirm the password");
+            flag = 1;
+        }
+        else if (etPassword.getText().toString().equals(etConfirm.getText().toString())) {
+            etConfirm.setError("This field don't match the password");
+            flag = 1;
+        }
+        return flag != 1;
+    }
+}

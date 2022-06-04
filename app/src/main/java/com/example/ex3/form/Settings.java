a9fd990f1cf823e00ce4afb1c063c7c54cb88247
Nastia Khizgiyaev <nastiakh28@gmail.com>
2022-06-04 13:19:23 +0300

    initial settings activity design

---
+++ app/src/main/java/com/example/ex3/form/Settings.java
@@ -0,0 +1,14 @@
+package com.example.ex3.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ex3.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
